462d33d67dbb5a8c88971784cecbb68e438ed3ca
Grant Staublin <grant.staublin@gmail.com>
2015-11-07 17:01:30 -0500

    Switching build tasks to gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,50 @@
 // Dependencies
 // -------------------------------------
 var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
+
+var clean = require('gulp-clean');
+
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
 var rename = require('gulp-rename');
 var clean = require('gulp-clean');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+var inject = require('gulp-inject');
+
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+
+var path = require('path');
+
+var browserSync = require('browser-sync');
+
+var notify = require('gulp-notify');
+
+var cp = require('child_process');
+
+
+
+// SVG + Inline
+// -------------------------------------
+gulp.task('svg', function() {
+  var svgs = gulp.src('static/svg/*.svg')
+  .pipe(svgmin(function(file) {
+    var prefix = path.basename(file.relative, path.extname(file.relative));
+    return {
+      plugins: [{
+        cleanupIDs: {
+          prefix: prefix + '-',
+          minify: true
+        }
+      }]
+    }
+  }))
+  .pipe(svgstore( { inlineSvg: true }))
+  .pipe(gulp.dest('_includes'))
+});
 
 
 
@@ -13,26 +52,92 @@ var sourcemaps = require('gulp-sourcemaps');
 // Clean
 // -------------------------------------
 gulp.task('clean', function(){
-  return gulp.src(['static/css'])
+  return gulp.src(['static/css', '_includes/svg.svg', '_site/'])
   .pipe(clean());
 });
 
 
-// Autoprefixer
+// Browsersync
 // -------------------------------------
-gulp.task('autoprefixer', function(){
-  return gulp.src('static/css/screen.css')
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(rename('screen.poop.css'))
-  .pipe(gulp.dest('static/css'))
-});
+// gulp.task('browser-sync', function() {
+//   browserSync({
+//       // we need to disable clicks and forms for when we test multiple rooms
+//       server : {},
+//       ghostMode: false
+//   });
+// });
 
 
-// Sass
+// Sass + PostCSS
 // -------------------------------------
 gulp.task('sass', function(){
+  var processors = [
+    autoprefixer( {browsers: ['last 2 versions']} ),
+    cssnano
+  ];
 
+ return gulp.src(['static/scss/**/*.scss'])
+ .pipe(sourcemaps.init())
+ .pipe( sass().on('error', sass.logError) )
+ .pipe(sourcemaps.init())
+ .pipe(postcss(processors))
+ .pipe(sourcemaps.write('./'))
+ .pipe(gulp.dest('static/css'))
+ .pipe(browserSync.stream());
 });
+
+
+// Build Jekyll Site
+// -------------------------------------
+gulp.task('jekyll-build', function (done) {
+  browserSync.notify('Building Jekyll');
+  return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+      .on('close', done);
+});
+
+
+// Rebuild Jekyll and Reload Page
+// -------------------------------------
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+  browserSync.reload();
+});
+
+
+// Wait for Jekyll Build and Launch Server
+// -------------------------------------
+gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
+});
+
+
+
+// xxxx
+// -------------------------------------
+
+
+// xxxx
+// -------------------------------------
+
+
+
+// // Watch
+// // -------------------------------------
+gulp.task('watch', function(){
+  gulp.watch('static/scss/**/*.scss', ['sass']);
+  gulp.watch(['*.html', '_layouts/*.html', '_posts/*', '_includes/*'], ['jekyll-rebuild']);
+});
+
+
+
+
+
+
+// Default
+// -------------------------------------
+// gulp.task('default', ['sass', 'watch', 'browser-sync']);
+
+gulp.task('default', ['browser-sync', 'watch']);
